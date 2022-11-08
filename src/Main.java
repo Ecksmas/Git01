8ee650f0f6d87b91f7a08baef51fc0ab019cd079
Ecksmas <ludde.oelker@gmail.com>
2022-11-08 16:25:49 +0100

    First branch commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("This is my first push and the incorrect one");
+        System.out.println("This is my first branch");
     }
 }
