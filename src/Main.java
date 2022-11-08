6f4d045febb73e98aa49b9c16d08deff6b19d3e9
Ecksmas <ludde.oelker@gmail.com>
2022-11-08 13:30:22 +0100

    Change number 3

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
+
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Hello user!");
-        System.out.println("Change1");
-
+        System.out.println("Change3");
     }
 }
